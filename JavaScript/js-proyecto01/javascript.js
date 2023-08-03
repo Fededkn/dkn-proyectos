76ec63ec369ab64608e0cda262d1c2113bcfe69c
Fededkn <federiconicolasfuentes@gmail.com>
2023-08-03 17:46:26 -0300

    version 6 - librerias y API

--- JavaScript/js-proyecto01/javascript.js
+++ JavaScript/js-proyecto01/javascript.js
@@ -1,365 +1,222 @@
+//Array para almacenar los activos
 let arrayActivos = [];
 
-class Activo {
-  constructor(ticker, nombre, cantidadPapeles, capitalInvertido) {
+  class Activo {
+    constructor(ticker, nombre, cantidadPapeles, capitalInvertido) {
       this.ticker = ticker;
       this.nombre = nombre;
       this.cantidadPapeles = cantidadPapeles;
       this.capitalInvertido = capitalInvertido;
       this.precioPromedioCompra = capitalInvertido / cantidadPapeles;
+    }
   }
-}
 
-function construirDivActivo(activo) {
-  // Crear un nuevo div
-  const divActivo = document.createElement("div");
-  divActivo.classList.add("activo");
+// Función para construir la fila de activo en la tabla
+const construirFilaActivo = (activo, index) => {
+  const { ticker, nombre, cantidadPapeles, precioPromedioCompra, capitalInvertido } = activo;
+  const filaActivo = document.createElement("tr");
 
-  // Construir el contenido del div con los datos del activo
-  divActivo.innerHTML = `
-      <p>${activo.ticker}</p>
-      <p>Nombre: ${activo.nombre}</p>
-      <p>Cantidad de Papeles: ${activo.cantidadPapeles}</p>
-      <p>Precio Promedio de Compra: ${activo.precioPromedioCompra.toFixed(2)}</p>
-      <p>Capital Invertido: ${activo.capitalInvertido}</p>
-      <button class="btnEliminar" indiceActivo="${arrayActivos.length}">Eliminar</button>
+  // Construir el contenido de la fila con los datos del activo.
+  filaActivo.innerHTML = `
+    <td>${ticker}</td>
+    <td>${nombre}</td>
+    <td>${cantidadPapeles}</td>
+    <td>${precioPromedioCompra.toFixed(2)}</td>
+    <td>${capitalInvertido}</td>
+    <td>
+        <button class="btnEliminar" indiceActivo="${index}">Eliminar</button>
+    </td>
   `;
 
-  // Agregar el div al contenedor de la lista de activos
+  // Agregar la fila al cuerpo de la tabla
   const listaActivos = document.getElementById("listaActivos");
-  listaActivos.appendChild(divActivo);
+  listaActivos.appendChild(filaActivo);
 
   // Agregar el evento de click al botón "Eliminar"
-  const btnEliminar = divActivo.querySelector(".btnEliminar");
+  const btnEliminar = filaActivo.querySelector(".btnEliminar");
   btnEliminar.addEventListener("click", eliminarActivo);
+};
 
-  // Actualizar el total invertido
-  calcularTotalInvertido();
-}
 
-// Función para eliminar un activo
-function eliminarActivo() {
-  // Obtener el índice del activo a eliminar del atributo indiceActivo del botón
-  const indiceActivo = parseInt(this.getAttribute("indiceActivo"));
+  function eliminarActivo() {
+    const indiceActivo = parseInt(this.getAttribute("indiceActivo"));
+    const activoAEliminar = arrayActivos[indiceActivo];
 
-  // Eliminar el activo del array
-  arrayActivos.splice(indiceActivo, 1);
+    // Confirmar eliminación
+    Swal.fire({
+      title: "¿Estás seguro?",
+      text: `Esta a punto de eliminar el activo: ${activoAEliminar.nombre}`,
+      showCancelButton: true,
+      cancelButtonText: "Cancelar",
+      confirmButtonText: "Eliminar",
 
-  // Actualizar la lista de activos en el HTML
-  mostrarActivos();
+    }).then((result) => {
+      if (result.isConfirmed) {
+        arrayActivos.splice(indiceActivo, 1);
+        mostrarActivos();
+        calcularTotalInvertido();
 
-  // Actualizar el total invertido
-  calcularTotalInvertido();
-}
+        Swal.fire({
+          title: "Eliminado",
+          text: `El activo ${activoAEliminar.nombre} ha sido eliminado.`,
+        });
+        mostrarGraficoPie()
+      }
+    });
 
-// Función para mostrar los activos en el HTML
-function mostrarActivos() {
-  const listaActivos = document.getElementById("listaActivos");
-  listaActivos.innerHTML = "";
+  }
 
-  arrayActivos.forEach((activo, index) => {
-      // Crear un nuevo div
-      const divActivo = document.createElement("div");
-      divActivo.classList.add("activo");
+  // Función para mostrar la lista de activos en la tabla
+  function mostrarActivos() {
+    const listaActivos = document.getElementById("listaActivos");
+    listaActivos.innerHTML = "";
 
-      // Construir el contenido del div con los datos del activo
-      divActivo.innerHTML = `
-      <span>${activo.ticker}</span>
-      <span>Nombre: ${activo.nombre}</span>
-      <span>Cantidad de Papeles: ${activo.cantidadPapeles}</span>
-      <span>Precio Promedio de Compra: ${activo.precioPromedioCompra.toFixed(2)}</span>
-      <span>Capital Invertido: ${activo.capitalInvertido}</span>
-      <button class="btnEliminar" indiceActivo="${index}">Eliminar</button>
-  `;
+    arrayActivos.forEach((activo, index) => {
+      construirFilaActivo(activo, index);
+    });
+    calcularTotalInvertido();
+  }
 
-      // Agregar el div al contenedor de la lista de activos
-      listaActivos.appendChild(divActivo);
+  // Función para calcular el total invertido
+  function calcularTotalInvertido() {
+    const totalInvertido = arrayActivos.reduce((sum, activo) => sum + activo.capitalInvertido, 0);
+    const totalInvertidoElement = document.getElementById("totalInvertido");
+    totalInvertidoElement.innerText = "Total invertido: " + totalInvertido.toFixed(2);
+  }
 
-      // Agregar el evento de click al botón "Eliminar"
-      const btnEliminar = divActivo.querySelector(".btnEliminar");
-      btnEliminar.addEventListener("click", eliminarActivo);
-  });
-}
+  // Función para guardar los activos en el almacenamiento local
+  function guardarActivos() {
+        // Confirmación para guardar activos en local storgate
+        Swal.fire({
+          text: `¿Desea guardar los cambios realizados?`,
+          showCancelButton: true,
+          cancelButtonText: "Cancelar",
+          confirmButtonText: "Guardar",
+    
+        }).then((result) => {
+          if (result.isConfirmed) {
+            localStorage.setItem("portafolioGuardar", JSON.stringify(arrayActivos));
+            Swal.fire({
+              title: "Guardado",
+              text: `El portafolio ha sido guardado con éxito.`,
+            });
+          }
+        });
+      }
+    
 
-// Función para calcular el total invertido
-function calcularTotalInvertido() {
-  const totalInvertido = arrayActivos.reduce((sum, activo) => sum + activo.capitalInvertido, 0);
-  const totalInvertidoElement = document.getElementById("totalInvertido");
-  totalInvertidoElement.innerText = "Total invertido: " + totalInvertido;
-}
-
-// Función para guardar los activos en el LocalStorage
-function guardarActivos() {
-  localStorage.setItem("portafolioGuardar", JSON.stringify(arrayActivos));
-}
-
-// Función para recuperar los activos del LocalStorage
-function recuperarActivos() {
-  const activosGuardados = localStorage.getItem("portafolioGuardar");
-  if (activosGuardados) {
+  // Función para recuperar los activos desde el local storgate
+  function recuperarActivos() {
+    const activosGuardados = localStorage.getItem("portafolioGuardar");
+    if (activosGuardados) {
       arrayActivos = JSON.parse(activosGuardados);
       mostrarActivos();
+      calcularTotalInvertido();
+      mostrarGraficoPie()
+    }
   }
-}
 
-// Función para manejar el evento de click en el botón "Agregar"
-function agregarActivo() {
-  // Obtener los valores de los campos del formulario
-  const ticker = document.getElementById("ticker").value;
-  const nombre = document.getElementById("nombre").value;
-  const cantidadPapeles = parseFloat(document.getElementById("cantidadPapeles").value);
-  const capitalInvertido = parseFloat(document.getElementById("capitalInvertido").value);
+  // Función para agregar un nuevo activo
+  function agregarActivo() {
+    const ticker = document.getElementById("ticker").value;
+    const nombre = document.getElementById("nombre").value;
+    const cantidadPapeles = parseFloat(document.getElementById("cantidadPapeles").value);
+    const capitalInvertido = parseFloat(document.getElementById("capitalInvertido").value);
 
-  // Validar que los campos no estén vacíos y que los valores sean números válidos
-  if (!ticker || !nombre || isNaN(cantidadPapeles) || isNaN(capitalInvertido) || cantidadPapeles <= 0 || capitalInvertido <= 0) {
-      alert("Por favor, complete todos los campos con valores válidos.");
+    if (!ticker || !nombre || isNaN(cantidadPapeles) || isNaN(capitalInvertido) || cantidadPapeles <= 0 || capitalInvertido <= 0) {
+      Swal.fire({
+        title: "Error",
+        text: "Por favor, complete todos los campos con valores válidos.",
+      });
       return;
+    }
+
+    const activo = new Activo(ticker, nombre, cantidadPapeles, capitalInvertido);
+    arrayActivos.push(activo);
+    mostrarActivos();
+
+    document.getElementById("ticker").value = "";
+    document.getElementById("nombre").value = "";
+    document.getElementById("cantidadPapeles").value = "";
+    document.getElementById("capitalInvertido").value = "";
+
+    calcularTotalInvertido();
+    mostrarGraficoPie()
   }
 
-  // Crear un objeto Activo con los valores obtenidos
-  const activo = new Activo(ticker, nombre, cantidadPapeles, capitalInvertido);
+// Variable para guardar la referencia al gráfico actual
+let graficoPie;
 
-  // Agregar el activo al array
-  arrayActivos.push(activo);
+// Función para mostrar el gráfico
+const mostrarGraficoPie = () => {
+  if (graficoPie) {
+    // Si hay un gráfico existente, se elimina para colocar el nuevo
+    graficoPie.destroy();
+  }
 
-  // Mostrar los activos en el HTML
-  mostrarActivos();
+  const datos = arrayActivos.map(activo => activo.capitalInvertido);
+  const nombres = arrayActivos.map(activo => activo.nombre);
 
-  // Limpiar los campos del formulario
-  document.getElementById("ticker").value = "";
-  document.getElementById("nombre").value = "";
-  document.getElementById("cantidadPapeles").value = "";
-  document.getElementById("capitalInvertido").value = "";
+  // Crear el gráfico utilizando la librería Chart.js
+  const ctx = document.getElementById("graficoPie").getContext("2d");
+  graficoPie = new Chart(ctx, {
+    type: "pie",
+    data: {
+      labels: nombres,
+      datasets: [{
+        data: datos,
+        backgroundColor: [
+          "rgb(255, 179, 186)", // Rosa
+          "rgb(189, 204, 255)", // Azul
+          "rgb(255, 236, 179)", // Amarillo
+          "rgb(178, 223, 138)", // Verde
+          "rgb(222, 184, 135)", // Marrón
+          "rgb(255, 204, 153)", // Naranja
+          "rgb(255, 204, 255)", // Lila
+        ],
+      }],
+    },
+    options: {
+      responsive: true,
+      legend: {
+        display: true,
+        position: "top",
+        fontSize: 30,
+      },
+    },
+  });
+};
 
-  // Actualizar el total invertido
-  calcularTotalInvertido();
-}
+// Obtener la cotización del dólar CCL desde la API externa
+fetch("https://criptoya.com/api/dolar")
+  .then((resp) => resp.json())
+  .then((data) => {
+    const cclValue = data.ccl;
+    // Mostrar el valor en el DOM
+    const cclElement = document.getElementById("cotizacion-ccl");
+    cclElement.innerText = "Cotización del dólar CCL: " + cclValue.toFixed(2);
+  })
+  .catch((error) => {
+    console.error("Error al obtener los datos:", error);
+  });
 
-// Asignar el evento de carga de la página para evitar la actualización
+// Inicializar Tippy.js en la celda del "Precio promedio de compra"
+tippy('#precioPromedioCompra', {
+  placement: 'top',
+  animation: 'scale',
+  content: 'Precio promedio de compra',
+});
+
+// Cuando el contenido del DOM ha sido cargado completamente, ejecutar las siguientes funciones:
 document.addEventListener("DOMContentLoaded", (e) => {
   e.preventDefault();
 
-  // Asignar el evento de click al botón "Agregar"
   document.getElementById("btnAgregar").addEventListener("click", agregarActivo);
-
-  // Asignar el evento de click al botón "Guardar cambios"
   document.getElementById("btnGuardar").addEventListener("click", guardarActivos);
-
-  // Asignar el evento de click al botón "Recuperar"
   document.getElementById("btnRecuperar").addEventListener("click", recuperarActivos);
+
+  recuperarActivos();
+  mostrarGraficoPie();
+  obtenerCotizacionCCL();
+  calcularTotalInvertido();
 });
-
-// --------------------------CODIO ANTERIO DE REFERENCIA--------
-
-
-// console.log("Herramientas a elegir:");
-// console.log("1. Calculadora de interés compuesto.");
-// console.log("2. Calculadora para tamaño de posición.");
-// console.log("3. Calculadora de precio para CEDEAR.");
-// console.log("4. Registrar operaciones.");
-
-// let activosArray = [];
-
-// class Activo {
-//   constructor(nombre, ticker, capitalInvertido, cantidadPapeles, tipoActivo) {
-//     this.nombre = nombre;
-//     this.ticker = ticker;
-//     this.capitalInvertido = capitalInvertido;
-//     this.cantidadPapeles = cantidadPapeles;
-//     this.tipoActivo = tipoActivo;
-//     this.precioPromedioCompra = capitalInvertido / cantidadPapeles;
-//   }
-// }
-
-// function activoAgregar() {
-//   let nombre = prompt("Ingrese el nombre del activo:");
-//   let ticker = prompt("Ingrese el ticker del activo:");
-//   let capitalInvertido = parseFloat(prompt("Ingrese el capital invertido en el activo:"));
-//   let cantidadPapeles = parseFloat(prompt("Ingrese la cantidad de papeles del activo:"));
-//   let tipoActivo = prompt("Ingrese el tipo de activo:");
-
-//   let activo = new Activo(nombre, ticker, capitalInvertido, cantidadPapeles, tipoActivo);
-//   activosArray.push(activo);
-// }
-
-// function activoEliminar() {
-//   let ticker = prompt("Ingrese el ticker del activo a eliminar:");
-
-//   for (let i = 0; i < activosArray.length; i++) {
-//     if (activosArray[i].ticker === ticker) {
-//       activosArray.splice(i, 1);
-//       console.log("Activo eliminado.");
-//       return;
-//     }
-//   }
-
-//   console.log("No se encontró ningún activo con ese nombre.");
-// }
-
-// function activoBuscar() {
-//   let nombre = prompt("Ingrese el nombre del activo a buscar:");
-
-//   for (let i = 0; i < activosArray.length; i++) {
-//     if (activosArray[i].nombre === nombre) {
-//       console.log(activosArray[i]);
-//       return;
-//     }
-//   }
-
-//   console.log("No se encontró ningún activo con ese nombre.");
-// }
-
-// let opcion = "";
-// while (opcion !== "salir") {
-//   opcion = prompt("Seleccione una herramienta:\n1. Calculadora de interes compuesto.\n2. Calculadora para tamaño de posición.\n3. Calculadora para precios de CEDEARs.\n4. Registrar Operaciones.\n5. Salir");
-//   switch (opcion) {
-//     case "1":
-
-//       ////////// Calculadora de interes compuesto INICIO
-
-//       console.log("Seleccionaste la Calculadora de interés compuesto:")
-//       var capital = parseFloat(prompt("Ingrese el capital a invertir:"));
-//       var tasa = parseFloat(prompt("Ingrese la tasa de interés:"));
-//       var reinversion = parseInt(prompt("Ingresar la cantidad de veces que se reinvierte el capital + interes"));
-//       var rendimiento = capital;
-
-//       for (var i = 0; i < reinversion; i++) {
-//         var interes = rendimiento * (tasa / 100);
-//         rendimiento += interes;
-//       }
-
-//       alert("Capital inicial: "+ capital.toString() + "\nTasa: "+ tasa.toString() + "\nPeriodos reinvertidos: "+ reinversion.toString() + "\nInterés generado: "+ (rendimiento - capital).toFixed(2).toString() + "\nTotal: " + rendimiento.toFixed(2) + "\nRendimiento %: " + ((rendimiento - capital)/capital * 100).toFixed(2))
-
-//       //Calculadora de interes compuesto FIN
-
-//       break;
-
-//     case "2":
-
-//       ////////// Calculadora para tamaño de posición INICIO
-
-//       console.log("Seleccionaste la calculadora para tamaño de posición:")
-
-//       let capitalInicial = parseFloat(prompt("Ingrese el monto del capital inicial:"));
-//       let capitalPerder = parseFloat(prompt("Ingrese el monto a arriesgar por operación:"));
-//       let stopLoss = parseFloat(prompt("Ingrese el valor del stop loss:"));
-//       let perdidaPorcentaje = (capitalPerder / capitalInicial) * 100;
-//       let capitalLimite = parseFloat(prompt("Ingresa porcentaje máximo de capital a comprometer:"))
-
-//       let capitalColocar = (capitalPerder * 100) / stopLoss;
-//       let capitalPorcentaje = (capitalColocar / capitalInicial) * 100;
-
-//       alert("Capital inicial: "+ capitalInicial.toString() + "\nStop Loss: "+ stopLoss.toFixed(2).toString() + "%" + "\nPérdida por stop loss: "+ capitalPerder.toFixed(2).toString() + ", y representa el " + perdidaPorcentaje.toFixed(2).toString() + "% del capital inicial." + "\nInvertir: "+ capitalColocar.toString() + " representa el " + capitalPorcentaje.toFixed(2).toString() + "% del capital inicial.")
-
-//       if (capitalPorcentaje > capitalLimite) {
-//         console.log("La inversión es grande, representa mas del 20% del capital incial.");
-//       } else {
-//         console.log("La inversión es baja, representa menos del 20% del capital inicial.");
-//       }
-
-//       //Calculadora para tamaño de posición FIN
-
-//       break;
-
-//     case "3":
-
-//       ////////// Calculadora para precios de CEDEARS INICIO
-
-//       console.log("Seleccionaste la calculadora para CEDEARS:")
-
-//       let precioSub = parseFloat(prompt("Ingrese el precio del subyacente:"));
-//       let precioCcl = parseFloat(prompt("Ingrese el precio del CCL:"));
-//       let ratio = parseFloat(prompt("Ingrese el ratio de conversión:"));
-
-//       function cedear(precioSub, precioCcl, ratio) {
-//         let precioCedear = (precioSub * precioCcl) / ratio;
-//         return precioCedear;
-//       }
-
-//       let resultado = cedear(precioSub, precioCcl, ratio)
-
-//       alert("El precio aproximado del CEDEAR consultado es de: " + resultado.toFixed(2))
-
-//       //Calculadora para precios de CEDEARS FIN
-
-//       break;
-
-//     case "4":
-
-//       ////////// Registrar operaciones
-
-//       console.log("Agregar acción/CEDEAR.");
-
-
-//       let opcionRegistro = "";
-
-//       while (opcionRegistro !== "salir") {
-//         opcionRegistro = prompt("Seleccione una opción:\n1. Activos registrados\n2. Agregar activo\n3. Eliminar activo\n4. Buscar activo\n5. Filtrar activos por tipo\n6. Salir");
-//         switch (opcionRegistro) {
-
-//           case "1":
-//             var contenido = activosArray.join(' ');
-//             alert('El contenido del array es:\n' + contenido)
-//             // for (let i = 0; i < activosArray.length; i++) {
-//             //   alert(activosArray[i])
-
-//             // }
-//             break;
-//           case "2":
-
-//             // AGREGAR ACTIVO
-//             activoAgregar();
-//             break;
-
-//           case "3":
-
-//             // ELIMINAR ACTIVO
-
-//             activoEliminar();
-//             break;
-
-//           case "4":
-
-//             // BUSCAR ACTIVO
-
-//             activoBuscar();
-//             break;
-
-//           case "5":
-
-//             // FILTRAR ACTIVO
-
-//             let tipo = prompt("Ingrese el tipo de activo por el que desea filtrar:");
-
-//             let activosFiltrados = activosArray.filter(activo => activo.tipoActivo === tipo);
-
-//             if (activosFiltrados.length > 0) {
-//               console.log("Activos encontrados del tipo", tipo + ":");
-//               activosFiltrados.forEach(activo => console.log(activo));
-//             } else {
-//               console.log("No se encontraron activos del tipo", tipo + ".");
-//             }
-//             break;
-
-//           case "6":
-
-//             opcionRegistro = "salir";
-//             break
-
-//           default:
-//             console.log("Opción inválida.");
-//             break;
-//         }
-//       }
-//       break;
-
-//     case "5":
-//       opcion = "salir";
-//       console.log("Programa finalizado.")
-//       break
-
-//     default:
-//       console.log("Opción inválida.")
-//       break;
-//   }
-// }
